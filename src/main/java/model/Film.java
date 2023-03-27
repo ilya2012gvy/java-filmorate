fd3fba1cd762d8f229c7efd37a4c2103c38a6515
ilya <ilea.morozov@yandex.ru>
2023-03-27 11:11:08 +0300

    Commit main

---
+++ src/main/java/model/Film.java
@@ -0,0 +1,14 @@
+package model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class Film {
+    private Integer id; // целочисленный идентификатор
+    private String name; // название
+    private String description; // описание
+    private LocalDate releaseDate; // дата релиза
+    private int duration; // продолжительность фильма
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,14 @@
+package model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class User {
+    private Integer id; // целочисленный идентификатор
+    private String email; // электронная почта
+    private String login; // логин пользователя
+    private String name; // название
+    private LocalDate birthday; // дата рождения
+}
